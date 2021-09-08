ce0107f980570ec890d40519d4281c286e7cab59
Keith Becker <keith.becker@recro.com>
2021-09-08 08:05:05 -0600

    update

---
+++ src/Medulla.Frontend/Client/Components/Editor/PropertiesWindow/Property.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+namespace Medulla.Frontend.Client.Components.Editor.PropertiesWindow
+{
+    public class Property
+    {
+
+        public string Name { get; set; }
+        public string InputDescription { get; set; }
+        public string DefaultValue { get; set; }
+        public string InputType { get; set; }
+
+    }
+}
