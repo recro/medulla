a022c6a2357b6d2e9a9a81efb6fbb8171fe2a339
Keith Becker <keith.becker@recro.com>
2021-09-08 09:57:41 -0600

    update

---
+++ src/Medulla.Frontend/Client/Components/Editor/PaletteWindow/PaletteWindowComponents.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace Medulla.Frontend.Client.Components.Editor.PaletteWindow
+{
+    public class PaletteWindowComponents
+    {
+        
+        public List<PaletteComponent> ComponentList { get; set; }
+        
+        
+    }
+}
