b71afb464ff93af8e080362679b7abc4ffdc9dda
Andrew Babbitt <andrew.babbitt@recro.com>
2021-09-13 12:48:28 -0600

    Fixed editor rendering

--- src/Medulla.Frontend/Client/Components/Editor/EditorView/EditorRenderer.cs
+++ src/Medulla.Frontend/Client/Components/Editor/EditorView/EditorRenderer.cs
@@ -2,22 +2,43 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 
-namespace Medulla.Frontend.Client.Components.Editor.EditorView
+namespace Medulla.Frontend.Client.Components.Editor.EditorView;
+
+public class EditorRenderer : ComponentBase
 {
-    public class EditorRenderer: ComponentBase
+    [Parameter]
+    public EditorViewNode EditorViewNode { get; set; } = default!;
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
-        
-        [Parameter]
-        public EditorStructure EditorStructure { get; set; } = default!;
-        
-        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        var type = Type.GetType(EditorViewNode.Type);
+
+        if (type == null)
+            throw new NullReferenceException("Type is not expected to be null. The RenderComponentType was not found.");
+
+        builder.OpenComponent(0, type);
+
+        foreach (var (key, value) in EditorViewNode.Parameters)
+            builder.AddAttribute(1, key, value);
+
+        builder.AddAttribute(1, "EditorViewNode", EditorViewNode);
+
+        if (EditorViewNode.Children.Count > 0)
         {
-            EditorStructure.EditorViewNode.Render(builder, EditorStructure);
+            builder.AddAttribute(2, "ChildContent", (RenderFragment)((childBuilder) =>
+            {
+                foreach (var child in EditorViewNode.Children)
+                {
+                    childBuilder.OpenComponent(0, typeof(EditorRenderer));
+                    childBuilder.AddAttribute(1, "EditorViewNode", child);
+                    childBuilder.CloseComponent();
+                }
+            }));
         }
+
+        builder.CloseComponent();
     }
 }
--- src/Medulla.Frontend/Client/Components/Editor/EditorView/EditorViewNode.cs
+++ src/Medulla.Frontend/Client/Components/Editor/EditorView/EditorViewNode.cs
@@ -2,50 +2,13 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Rendering;
+namespace Medulla.Frontend.Client.Components.Editor.EditorView;
 
-namespace Medulla.Frontend.Client.Components.Editor.EditorView
+public class EditorViewNode
 {
-    public class EditorViewNode
-    {
-        public readonly List<EditorViewNode> Children = new();
+    public string Type { get; set; } = "RenderFragment";
 
-        public string RenderComponentType { get; set; } = "RenderFragment";
+    public readonly List<EditorViewNode> Children = new();
 
-        public Dictionary<string, object> Parameters { get; set; } = new();
-
-        public void Render(RenderTreeBuilder builder, EditorStructure editorStructure)
-        {
-            var type = Type.GetType(RenderComponentType);
-            if (type == null)
-            {
-                throw new NullReferenceException(
-                    "Type is not expected to be null. The RenderComponentType was not found.");
-            }
-
-            builder.OpenComponent(0, type);
-            
-            foreach (var (key, value) in Parameters)
-            {
-                builder.AddAttribute(1, key, value);
-            }
-            
-            builder.AddAttribute(1, "EditorStructure", editorStructure);
-            
-            builder.AddAttribute(2, "ChildContent", (RenderFragment)((builder2) =>
-            {
-                foreach (var child in Children)
-                {
-                    child.Render(builder2, editorStructure);
-                }
-            }));
-
-            builder.CloseComponent();
-        }
-        
-
-    }
+    public Dictionary<string, object> Parameters { get; set; } = new();
 }
