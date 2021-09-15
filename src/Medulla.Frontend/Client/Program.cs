1ed67c42c5593580087275ff15e8c0e3748302b4
Andrew Babbitt <andrew.babbitt@recro.com>
2021-09-14 18:33:22 -0600

    chore: update to .NET 6 RC 1 (#16)

--- src/Medulla.Frontend/Client/Program.cs
+++ src/Medulla.Frontend/Client/Program.cs
@@ -3,9 +3,11 @@
 // See the LICENSE file in the project root for more information.
 
 using Medulla.Frontend.Client;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 await builder.Build().RunAsync();
