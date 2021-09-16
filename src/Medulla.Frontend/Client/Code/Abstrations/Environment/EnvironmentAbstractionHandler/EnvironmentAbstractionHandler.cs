e13c66f52a82b7bb4db2926395a965d6a24bfa7d
Keith Becker <keith.becker@recro.com>
2021-09-16 08:40:11 -0600

    Adding EnvironmentAbstractionHandler.cs

---
+++ src/Medulla.Frontend/Client/Code/Abstrations/Environment/EnvironmentAbstractionHandler/EnvironmentAbstractionHandler.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Medulla.Frontend.Client.Code
+{
+    abstract public class EnvironmentAbstractionHandler
+    {
+
+        abstract protected void HandleInEditor();
+
+        abstract protected void HandleInProduction();
+
+        abstract protected void HandleInDevelopment();
+
+        public enum Environment
+        {
+            Editor,
+            Production,
+            Development
+        }
+
+        public void Handle()
+        {
+            Environment env = GetCurrentEnvironment();
+            if (env == Environment.Development)
+            {
+                HandleInDevelopment();
+            } else if (env == Environment.Editor)
+            {
+                HandleInEditor();
+            } else if (env == Environment.Production)
+            {
+                HandleInProduction();
+            }
+        }
+
+        public static Environment GetCurrentEnvironment()
+        {
+            //code that figures out the environment
+            //TODO implement this function
+            return Environment.Editor;
+        }
+
+    }
+}
