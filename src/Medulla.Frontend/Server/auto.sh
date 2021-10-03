47d1811eec94d10e02f370b481072c1df858222d
root <root@ip-172-31-27-45.us-east-2.compute.internal>
2021-10-03 17:29:23 +0000

    auto.sh

--- src/Medulla.Frontend/Server/auto.sh
+++ src/Medulla.Frontend/Server/auto.sh
@@ -1,10 +1,14 @@
 
-sudo su root
+docker stop medulla
+docker rmi medulla -f
+
+
 cd ~
 cd medulla/medulla
 git pull
 
-./build-docker.sh
+cd ~/medulla/medulla/src/Medulla.Frontend/Server
+docker build -t medulla -f ./Dockerfile ../..
 
 docker stop medulla
 docker run --rm -d -p 80:80 --name=medulla medulla 
