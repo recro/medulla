bb5ad31c9e4e6f679d98a1014215a6019ce83877
Keith Becker <keith.becker@recro.com>
2021-10-19 07:34:55 -0600

    align spacing and import type

--- src/Medulla.Frontend/Client/Components/RegisteredComponents/BaseComponentContainer.cs
+++ src/Medulla.Frontend/Client/Components/RegisteredComponents/BaseComponentContainer.cs
@@ -2,6 +2,8 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+
 namespace Medulla.Frontend.Client.Components.RegisteredComponents
 {
     public abstract class BaseComponentContainer : BaseComponent
