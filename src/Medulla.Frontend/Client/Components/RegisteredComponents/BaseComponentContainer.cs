f8f43ff71bacb3b2e3ab607a9ca1d5c69fbe49c7
Keith Becker <keith.becker@recro.com>
2021-10-20 00:24:47 -0600

    update

--- src/Medulla.Frontend/Client/Components/RegisteredComponents/BaseComponentContainer.cs
+++ src/Medulla.Frontend/Client/Components/RegisteredComponents/BaseComponentContainer.cs
@@ -2,15 +2,32 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 
 namespace Medulla.Frontend.Client.Components.RegisteredComponents
 {
     public abstract class BaseComponentContainer : BaseComponent
     {
+        
+        
+        protected string hoverClass = "";
+
+        protected void HandleDragEnter()
+        {
+            hoverClass = "hovering";
+        }
+
+        protected void HandleDragLeave()
+        {
+            hoverClass = "";
+        }
+
 
         protected void HandleDrop()
         {
+            hoverClass = "";
+            Console.WriteLine("handling drop");
             Editor.PlaceInUniqueId = this.UniqueId;
             Editor.AddComponentToEditorViewNode(Editor.EditorViewNode);
             foreach (var child in GetChildrenToAddOnAdd())
