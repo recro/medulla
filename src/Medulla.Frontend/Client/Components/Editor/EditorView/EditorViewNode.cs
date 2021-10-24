6c6062afdb89c024f5d438b6aca1459ebcc7a020
Keith Becker <keith.becker@recro.com>
2021-10-23 18:02:08 -0600

    update

--- src/Medulla.Frontend/Client/Components/Editor/EditorView/EditorViewNode.cs
+++ src/Medulla.Frontend/Client/Components/Editor/EditorView/EditorViewNode.cs
@@ -2,8 +2,11 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Medulla.Frontend.Client.Library.Utilities.Unique;
+using System.Text.Json;
 
 
 public class EditorViewNode
@@ -19,5 +22,29 @@ public class EditorViewNode
     public UniqueId? UniqueId { get; set; }
 
     public bool IsContainer { get; set; } = false;
+
+
+    public string GetJson()
+    {
+        List<string> childrenJson = new List<string>();
+        for (var i = 0; i < Children.Count; i++)
+        {
+            string childJson = Children[i].GetJson();
+            childrenJson.Add(childJson);
+        }
+
+        string childrenJsonObjects = String.Join(',', childrenJson);
+        string childrenJsonArray = $"[ {childrenJsonObjects} ]";
+        string json = JsonSerializer.Serialize(Parameters);
+
+        return $"{{  " +
+               $"\"Type\": \"{Type}\", " +
+               $"\"Children\": {childrenJsonArray}, " +
+               $"\"Parameters\": {json}, " +
+               $"\"UniqueId\": \"{UniqueId.Id}\", " +
+               $"\"IsContainer\": {IsContainer.ToString().ToLower()} " +
+               $" }}";
+        
+    }
 
 }
---
+++ src/Medulla.Frontend/Client/Library/Abstractions/Environment/Page.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+namespace Medulla.Frontend.Client.Library.Abstractions.Environment
+{
+    public class Page
+    {
+
+        private static Page? s_instance;
+
+        public EditorViewNode EditorViewNode { get; set; } = new();
+
+        public static Page GetInstance()
+        {
+            if (Page.s_instance == null)
+            {
+                Page.s_instance = new Page();
+            }
+            return Page.s_instance;
+        }
+
+        public static void UpdateInstance(Page page)
+        {
+            Page.s_instance = page;
+        }
+
+    }
+}
