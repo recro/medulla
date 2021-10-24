54f8c59344ce33b346ba3e3d9c00a37e9b5c9a53
Keith Becker <keith.becker@recro.com>
2021-10-24 09:42:38 -0600

    update

--- appspec_scripts/ApplicationStart.sh
+++ appspec_scripts/ApplicationStart.sh
@@ -2,9 +2,9 @@
 
 echo "running"
 
+docker login --username AWS --password="$(cat /tmp/docker-password)" 369840493607.dkr.ecr.us-east-2.amazonaws.com
+docker pull 369840493607.dkr.ecr.us-east-2.amazonaws.com/medulla-frontend:latest
 docker stop medulla
 docker rm medulla
+docker run -d --name=medulla --restart=always -p 80:5000 369840493607.dkr.ecr.us-east-2.amazonaws.com/medulla-frontend:latest
 docker rmi 369840493607.dkr.ecr.us-east-2.amazonaws.com/medulla-frontend:latest
-docker pull 369840493607.dkr.ecr.us-east-2.amazonaws.com/medulla-frontend:latest
-docker run --rm -d --name=medulla -p 80:5000 369840493607.dkr.ecr.us-east-2.amazonaws.com/medulla-frontend:latest
-
