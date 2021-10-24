bb02f7ea6f92958c2805a2cbcbd7de363634836b
Keith Becker <keith.becker@recro.com>
2021-10-24 09:57:45 -0600

    update

--- appspec_scripts/ApplicationStart.sh
+++ appspec_scripts/ApplicationStart.sh
@@ -7,4 +7,3 @@ docker pull 369840493607.dkr.ecr.us-east-2.amazonaws.com/medulla-frontend:latest
 docker stop medulla
 docker rm medulla
 docker run -d --name=medulla --restart=always -p 80:5000 369840493607.dkr.ecr.us-east-2.amazonaws.com/medulla-frontend:latest
-docker rmi 369840493607.dkr.ecr.us-east-2.amazonaws.com/medulla-frontend:latest
