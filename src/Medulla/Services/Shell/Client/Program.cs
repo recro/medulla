4b4fc86e547934a8828ec8198cabc892ad1a3d33
Andrew Babbitt <andrew.babbitt@recro.com>
2022-01-28 02:13:13 -0700

    Renamed Frontend to Shell

--- src/Medulla/Services/Shell/Client/Program.cs
+++ src/Medulla/Services/Shell/Client/Program.cs
@@ -2,7 +2,7 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
-using Medulla.Services.Frontend.Client;
+using Medulla.Services.Shell.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
