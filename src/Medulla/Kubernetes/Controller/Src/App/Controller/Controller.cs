d708ce9276562f5419b99d5faee0a367a1df8867
Keith Becker <keith.becker@recro.com>
2022-01-29 15:37:16 -0700

    update

--- src/Medulla/Kubernetes/Controller/Src/App/Controller/Controller.cs
+++ src/Medulla/Kubernetes/Controller/Src/App/Controller/Controller.cs
@@ -1,4 +1,5 @@
 using App.Utils;
+using k8s;
 
 namespace App.Controller;
 
@@ -8,13 +9,25 @@ public sealed class Controller {
 
         AppOptionsSingleton appOptions = AppOptionsSingleton.GetInstance();
 
+        KubernetesClientConfiguration config;
+
         if (appOptions.IsKubernetesConfigInCluster()) {
             Logger.Message("Kube config is in-cluster");
+            config = KubernetesClientConfiguration.InClusterConfig();
 
         } else if (appOptions.IsKubernetesConfigLocal()) {
             Logger.Message("Kube config is local");
-
+            config = KubernetesClientConfiguration.BuildDefaultConfig();
+        } else {
+            throw new System.Exception("Failed not appOptions.IsKubernetesConfigInCluster or appOptions.IsKubernetesConfigLocal");
         }
+
+        Controller.StartWithConfig(config);
+
+    }
+
+
+    public static void StartWithConfig(KubernetesClientConfiguration config) {
 
     }
 
