bd121ce85d13eac2dde30fec79c9731ded70e1db
Keith Becker <keith.becker@recro.com>
2022-02-04 07:16:12 -0700

    update

--- src/Medulla/Kubernetes/Controller/src/dapr-state.ts
+++ src/Medulla/Kubernetes/Controller/src/dapr-state.ts
@@ -1,6 +1,4 @@
-
 import axios from "axios";
-
 
 const setState = async (key :string, value :any) => {
     await axios.post(`http://${process.env.DAPR_HOST}:${process.env.DAPR_HTTP_PORT}/v1.0/state/statestore`, [{ "key": key, "value": value}]);
@@ -10,8 +8,6 @@ const getState = async (key :string) => {
     const response = await axios.get(`http://localhost:3500/v1.0/state/statestore/${key}`)
     return response.data;
 }
-
-
 
 export {
     getState,
