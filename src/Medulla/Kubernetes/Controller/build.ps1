80bc38af8709dc624377d9b5acc8a23b7a8c9d78
Keith Becker <keith.becker@recro.com>
2022-02-04 07:09:33 -0700

    update

--- src/Medulla/Kubernetes/Controller/build.ps1
+++
@@ -1,4 +0,0 @@
-aws ecr --profile=recro get-login-password --region us-east-2 | docker login --username AWS --password-stdin 369840493607.dkr.ecr.us-east-2.amazonaws.com
-docker build -t medulla-crd-kubernetes-controller .
-docker tag medulla-crd-kubernetes-controller:latest 369840493607.dkr.ecr.us-east-2.amazonaws.com/medulla-crd-kubernetes-controller:latest
-docker push 369840493607.dkr.ecr.us-east-2.amazonaws.com/medulla-crd-kubernetes-controller:latest
