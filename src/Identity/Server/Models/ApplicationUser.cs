cbe19f2c2d249741b9ffc9c471a5715a43c0bdf9
Andrew Babbitt <andrew.babbitt@recro.com>
2022-03-04 21:39:40 -0700

    feat: identity server

---
+++ src/Identity/Server/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Medulla.Identity.Server.Models;
+
+/// <summary>
+/// Represents an application user.
+/// </summary>
+public class ApplicationUser : IdentityUser { }
