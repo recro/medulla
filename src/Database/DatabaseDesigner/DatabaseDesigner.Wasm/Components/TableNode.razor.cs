d6261178c7afa020dde9e9c4edceace04e23558c
Keith Becker <keith.becker@recro.com>
2022-04-04 14:15:41 -0600

    update

---
+++ src/Database/DatabaseDesigner/DatabaseDesigner.Wasm/Components/TableNode.razor.cs
@@ -0,0 +1,11 @@
+﻿using DatabaseDesigner.Core.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DatabaseDesigner.Wasm.Components
+{
+    public partial class TableNode
+    {
+        [Parameter]
+        public Table Node { get; set; }
+    }
+}
---
+++ src/Database/DatabaseDesigner/README.md
@@ -0,0 +1,5 @@
+# DatabaseDesigner
+ 
+A sample project that demonstrates how Blazor and [Z.Blazor.Diagrams](https://github.com/zHaytam/Blazor.Diagrams) can be used.
+
+![](DBDesigner.png)
