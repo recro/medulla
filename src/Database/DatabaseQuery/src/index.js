ff054b2b0f03b1ad47fd38ea28da75f155d27621
Keith Becker <keith.becker@recro.com>
2022-04-05 16:22:45 -0600

    grpc/grpc-js

--- src/Database/DatabaseQuery/src/index.js
+++ src/Database/DatabaseQuery/src/index.js
@@ -1,3 +1,17 @@
 
+let PROTO_PATH = __dirname + '/query.proto';
+let grpc = require('@grpc/grpc-js');
+let protoLoader = require('@grpc/proto-loader');
 
 // implement grpc api and sequelize
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+
+console.log(packageDefinition);
+
