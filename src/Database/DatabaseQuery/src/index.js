9f67b9b62aa402cdfcd8e2146bf2926555731827
Keith Becker <keith.becker@recro.com>
2022-04-05 16:39:44 -0600

    starting server

--- src/Database/DatabaseQuery/src/index.js
+++ src/Database/DatabaseQuery/src/index.js
@@ -4,7 +4,7 @@ let grpc = require('@grpc/grpc-js');
 let protoLoader = require('@grpc/proto-loader');
 
 // implement grpc api and sequelize
-var packageDefinition = protoLoader.loadSync(
+let packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
     {keepCase: true,
         longs: String,
@@ -13,5 +13,49 @@ var packageDefinition = protoLoader.loadSync(
         oneofs: true
     });
 
+let protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+// The protoDescriptor object has the full package hierarchy
+console.log(protoDescriptor)
+let routeguide = protoDescriptor.routeguide;
+
 console.log(packageDefinition);
+
+
+function Create() {
+    
+}
+
+function Get() {
+    
+}
+
+function Delete() {
+    
+}
+
+function Update() {
+    
+}
+
+
+function getServer() {
+    var server = new grpc.Server();
+    server.addService(protoDescriptor.DbQuery.service, {
+        Create,
+        Get,
+        Delete,
+        Update,
+    });
+    return server;
+}
+
+
+
+var routeServer = getServer();
+routeServer.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    console.log("starting server")
+    routeServer.start();
+});
+
+
 
