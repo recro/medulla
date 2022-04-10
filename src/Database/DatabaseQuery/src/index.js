0c4960a7772f5e222a3ee9bcb79e5fcd51e2ed1a
Keith Becker <keith.becker@recro.com>
2022-04-10 10:03:04 -0600

    query.proto

--- src/Database/DatabaseQuery/src/index.js
+++ src/Database/DatabaseQuery/src/index.js
@@ -2,6 +2,7 @@
 let PROTO_PATH = __dirname + '/query.proto';
 let grpc = require('@grpc/grpc-js');
 let protoLoader = require('@grpc/proto-loader');
+const { Sequelize, Model, DataTypes } = require('sequelize');
 
 // implement grpc api and sequelize
 let packageDefinition = protoLoader.loadSync(
@@ -20,10 +21,47 @@ let routeguide = protoDescriptor.routeguide;
 
 console.log(packageDefinition);
 
+async function dbConnect(database, user, password, host, dialect) {
+    const sequelize = new Sequelize(database, user, password, {
+        host,
+        dialect
+    });
+
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        throw error
+    }
+    
+    return sequelize
+}
+
 
 
 function Create(call, callback) {
     console.log(call.request)
+
+    const { Database, User, Password, Host, Dialect } = call.request
+    let sequelize = dbConnect(Database, User, Password, Host, Dialect)
+    
+    const data = call.request
+    
+    
+    let modelObject = {}
+    let keys = 
+
+    const User = sequelize.define(data.Model.ModelName, {
+        name: DataTypes.TEXT,
+        favoriteColor: {
+            type: DataTypes.TEXT,
+            defaultValue: 'green'
+        },
+        age: DataTypes.INTEGER,
+        cash: DataTypes.INTEGER
+    });
+    
     callback(null, { Model: "test", Database: "test database" })
 }
 
