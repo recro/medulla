c7e173de1d7f942a2f595cbb9f0481c386498938
Keith Becker <keith.becker@recro.com>
2022-04-10 10:42:26 -0600

    iLogger

--- src/Controller/Controller/Controllers/DataCtrl.cs
+++ src/Controller/Controller/Controllers/DataCtrl.cs
@@ -62,20 +62,22 @@ internal class OnChange
 [EntityRbac(typeof(V1Alpha1DataEntity), Verbs = RbacVerb.All)]
 public class DataCtrl : IResourceController<V1Alpha1DataEntity>
 {
-    
+
+    private ILogger<DataCtrl> _iLogger { get; set; }
+
     public Task<ResourceControllerResult> CreatedAsync(V1Alpha1DataEntity resource)
     {
-        Console.WriteLine("Created");
+        _iLogger.LogInformation("Created");
 
-        Console.WriteLine("creating channel");
+        _iLogger.LogInformation("creating channel");
         var channel = GrpcChannel.ForAddress("http://localhost:5188");
-        Console.WriteLine("Creating client");
+        _iLogger.LogInformation("Creating client");
         var client = new DatabaseSvc.DatabaseSvcClient(channel);
-        Console.WriteLine("Client created");
+        _iLogger.LogInformation("Client created");
 
         for (var i = 0; i < resource.Spec.Count; i++)
         {
-            Console.WriteLine("Sending request to Create Database Resources");
+            _iLogger.LogInformation("Sending request to Create Database Resources");
             client.CreateDatabaseResources(new CreateDatabaseResourcesRequest()
             {
                 Name = resource.Metadata.Name,
@@ -86,7 +88,7 @@ public class DataCtrl : IResourceController<V1Alpha1DataEntity>
             }, new CallOptions() { });
         }
         
-        Console.WriteLine("Waiting for 5 seconds for resources to be created");
+        _iLogger.LogInformation("Waiting for 5 seconds for resources to be created");
         System.Threading.Thread.Sleep(5000);
         
         OnChange.UpdateDatabase(resource);
@@ -96,17 +98,17 @@ public class DataCtrl : IResourceController<V1Alpha1DataEntity>
 
     public Task<ResourceControllerResult> ReconcileAsync(V1Alpha1DataEntity resource)
     {
-        Console.WriteLine("Created");
+        _iLogger.LogInformation("Created");
 
-        Console.WriteLine("creating channel");
+        _iLogger.LogInformation("creating channel");
         var channel = GrpcChannel.ForAddress("http://internal-database-service:5188");
-        Console.WriteLine("Creating client");
+        _iLogger.LogInformation("Creating client");
         var client = new DatabaseSvc.DatabaseSvcClient(channel);
-        Console.WriteLine("Client created");
+        _iLogger.LogInformation("Client created");
 
         for (var i = 0; i < resource.Spec.Count; i++)
         {
-            Console.WriteLine("Sending request to Create Database Resources");
+            _iLogger.LogInformation("Sending request to Create Database Resources");
             client.CreateDatabaseResources(new CreateDatabaseResourcesRequest()
             {
                 Name = resource.Metadata.Name,
@@ -117,7 +119,7 @@ public class DataCtrl : IResourceController<V1Alpha1DataEntity>
             }, new CallOptions() { });
         }
         
-        Console.WriteLine("Waiting for 5 seconds for resources to be created");
+        _iLogger.LogInformation("Waiting for 5 seconds for resources to be created");
         System.Threading.Thread.Sleep(5000);
         
         OnChange.UpdateDatabase(resource);
@@ -127,14 +129,14 @@ public class DataCtrl : IResourceController<V1Alpha1DataEntity>
 
     public Task<ResourceControllerResult> StatusModifiedAsync(V1Alpha1DataEntity resource)
     {
-        Console.WriteLine("StatusModifiedAsync");
+        _iLogger.LogInformation("StatusModifiedAsync");
         OnChange.UpdateDatabase(resource);
         return Task.FromResult<ResourceControllerResult>(null);
     }
 
     public Task<ResourceControllerResult> DeletedAsync(V1Alpha1DataEntity resource)
     {
-        Console.WriteLine("DeletedAsync");
+        _iLogger.LogInformation("DeletedAsync");
         return Task.FromResult<ResourceControllerResult>(null);
     }
 
