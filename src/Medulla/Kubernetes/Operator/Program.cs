b677578023a15796e595922519c3562223c3453b
Keith Becker <keith.becker@recro.com>
2022-04-11 14:52:08 -0600

    Controllers/DataCtrl.cs

--- src/Medulla/Kubernetes/Operator/Program.cs
+++ src/Medulla/Kubernetes/Operator/Program.cs
@@ -1,4 +1,4 @@
-// Licensed to the Medulla Contributors under one or more agreements.
+﻿// Licensed to the Medulla Contributors under one or more agreements.
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
