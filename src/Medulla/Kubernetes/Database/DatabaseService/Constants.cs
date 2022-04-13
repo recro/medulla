a97053d72218495ddb497766ff627e41d96b4b41
Keith Becker <keith.becker@recro.com>
2022-04-13 10:05:40 -0600

    Constants.cs

--- src/Medulla/Kubernetes/Database/DatabaseService/Constants.cs
+++ src/Medulla/Kubernetes/Database/DatabaseService/Constants.cs
@@ -4,7 +4,14 @@
 
 namespace DatabaseService;
 
+/// <summary>
+/// This class is a place to define constants that are used throughout the project with one place to change values.
+/// </summary>
 public class Constants
 {
+    /// <summary>
+    /// This constant controls how many characters will show up in kubernetes object names for database names.
+    /// These will be random strings.
+    /// </summary>
     public static int LENGTH_OF_DATABASE_RANDOM_STRING = 10;
 }
