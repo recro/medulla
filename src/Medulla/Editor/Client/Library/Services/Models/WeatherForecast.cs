15529f9f8d998ca01bdd0c4949e7c4eb7a9841ae
Keith Becker <keith.becker@recro.com>
2022-05-02 16:23:11 -0600

    update

--- src/Medulla/Editor/Client/Library/Services/Models/WeatherForecast.cs
+++ src/Medulla/Editor/Client/Library/Services/Models/WeatherForecast.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 
 namespace Medulla.Frontend.Client.Library.Services.Models
 {
