15a85e6ac2d9a1330faeea1f7497ff9079ea2167
Baban Faraj <baban.faraj@recro.com>
2022-06-15 11:23:26 -0400

    Create controller for application enttiy

---
+++ src/Medulla/Kubernetes/Operator/Controllers/ApplicationCtrl.cs
@@ -0,0 +1,60 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+
+using ApplicationControllerKubeOps.Controller.Entities;
+
+
+namespace ApplicationControllerKubeOps.Controller.Controllers;
+
+
+/// <summary>
+/// ApplicationCtrl class for KubeOps Application CRD
+/// </summary>
+[EntityRbac(typeof(V1Alpha1ApplicationEntity), Verbs = RbacVerb.All)
+public class ApplicationCtrl : IResourceController<V1Alpha1ApplicationEntity>
+{
+    /// <summary>
+    /// CreatedAsync called by KubeOps when CRD created
+    /// </summary>
+    /// <param name="resource">crd resource</param>
+    /// <returns></returns>
+    public Task<ResourceControllerResult?> CreatedAsync(V1Alpha1ApplicationEntity resource)
+    {
+        return Task.FromResult<ResourceControllerResult>(null!)!;
+    }
+
+    /// <summary>
+    /// ReconcileAsync called by KubeOps when resource updated
+    /// </summary>
+    /// <param name="resource">KubeOps CRD resource</param>
+    /// <returns></returns>
+    public Task<ResourceControllerResult?> ReconcileAsync(V1Alpha1ApplicationEntity resource)
+    {
+        return Task.FromResult<ResourceControllerResult>(null!)!;
+    }
+
+    /// <summary>
+    /// StatusModifiedAsync called by KubeOps when status modified
+    /// </summary>
+    /// <param name="resource">KubeOps CRD resource</param>
+    /// <returns></returns>
+    public Task<ResourceControllerResult> StatusModifiedAsync(V1Alpha1ApplicationEntity resource)
+    {
+        Console.WriteLine("StatusModifiedAsync");
+        return Task.FromResult<ResourceControllerResult>(null!)!;
+    }
+
+    /// <summary>
+    /// DeletedAsync Called by KubeOps when resource deleted
+    /// </summary>
+    /// <param name="resource">KubeOps CRD Resource</param>
+    /// <returns></returns>
+    public Task<ResourceControllerResult> DeletedAsync(V1Alpha1ApplicationEntity resource)
+    {
+        Console.WriteLine("DeletedAsync");
+        return Task.FromResult<ResourceControllerResult>(null!)!;
+    }
+}
+
