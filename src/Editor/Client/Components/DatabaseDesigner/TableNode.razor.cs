c6457c7eb3237f2228105e7d202ab88aee8d88f7
Keith Becker <keith.becker@recro.com>
2022-06-15 15:38:06 -0600

    Deleted

--- src/Editor/Client/Components/DatabaseDesigner/TableNode.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace Medulla.Editor.Client.Components.DatabaseDesigner;
-
-public partial class TableNode
-{
-    [Parameter]
-    public Table? Node { get; set; }
-}
