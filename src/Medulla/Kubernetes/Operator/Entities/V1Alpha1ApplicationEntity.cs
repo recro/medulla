dae14f243557c64837d31a1545b34aa6577317eb
Keith Becker <keith.becker@recro.com>
2022-06-16 09:56:17 -0600

    feat: KubeOps

---
+++ src/Medulla/Kubernetes/Operator/Entities/V1Alpha1ApplicationEntity.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+using k8s.Models;
+using KubeOps.Operator.Entities;
+using KubeOps.Operator.Entities.Annotations;
+
+namespace ApplicationControllerKubeOps.Controller.Entities;
+
+
+/// <summary>
+/// ApplicationSpec is spec of CRD for application
+/// </summary>
+public struct ApplicationSpec
+{
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? Description { get; set; }
+}
+
+/// <summary>
+/// V1Alpha1DataEntity is CRD for data
+/// </summary>
+[Description("A CustomResourceDefinition which allows building with data in Medulla.")]
+[KubernetesEntity(
+    ApiVersion = "v1alpha1",
+    Kind = "Application",
+    Group = "medulla.recro.com",
+    PluralName = "application")]
+public class V1Alpha1ApplicationEntity : CustomKubernetesEntity
+{
+    public List<ApplicationSpec>? Spec { get; set; }
+}
+
