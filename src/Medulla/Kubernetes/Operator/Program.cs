942f856f1a01a9f02e643b9931b69ed5b1a77328
Baban Faraj <baban.faraj@recro.com>
2022-06-17 13:36:48 -0400

    Updated Program.cs by utilizing aspnetcore-6.0 top level feature to reduce the amount of code used in program.cs and startup.cs

--- src/Medulla/Kubernetes/Operator/Program.cs
+++ src/Medulla/Kubernetes/Operator/Program.cs
@@ -2,43 +2,12 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections;
-using System.Threading.Tasks;
-using Controller;
 using KubeOps.Operator;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Operator;
 
+var builder =  WebApplication.CreateBuilder(args);
+builder.Services.AddKubernetesOperator();
+var app = builder.Build();
+app.UseKubernetesOperator();
+app.RunOperatorAsync(args);
 
-namespace DatabaseControllerKubeOps;
-
-
-
-public static class Program
-{
-    /// <summary>
-    /// Main
-    /// </summary>
-    /// <param name="args"></param>
-    /// <returns></returns>
-    public static Task<int> Main(string[] args)
-    {
-
-        return CreateHostBuilder(args)
-            .Build()
-            .RunOperatorAsync(args);
-    }
-
-    private static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-    }
-}
