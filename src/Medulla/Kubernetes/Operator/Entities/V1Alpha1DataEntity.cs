4f19dcffa9af604ad51876849dde39460fd3cdf8
Baban Faraj <baban.faraj@recro.com>
2022-06-21 14:29:17 -0400

    Upadte namespace

--- src/Medulla/Kubernetes/Operator/Entities/V1Alpha1DataEntity.cs
+++ src/Medulla/Kubernetes/Operator/Entities/V1Alpha1DataEntity.cs
@@ -5,7 +5,7 @@
 using k8s.Models;
 using KubeOps.Operator.Entities;
 using KubeOps.Operator.Entities.Annotations;
-using Operator.model;
+using Operator.Model;
 
 namespace Medulla.Kubernetes.Operator.Entities;
 
