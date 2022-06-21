02fccbbde64d2f26f991583be6e62806b9033537
Baban Faraj <baban.faraj@recro.com>
2022-06-21 11:22:23 -0400

    Add comment to public elements

--- src/Medulla/Kubernetes/Operator/Entities/V1Alpha1DataEntity.cs
+++ src/Medulla/Kubernetes/Operator/Entities/V1Alpha1DataEntity.cs
@@ -15,16 +15,34 @@ namespace Medulla.Kubernetes.Operator.Entities;
 /// </summary>
 public struct DatabaseSpec
 {
+    /// <summary>
+    /// Name is an element of DatabaseSpec which is the name of the database
+    /// </summary>
     [Required]
     public string? Name { get; set; }
+    /// <summary>
+    /// Host is an element of DatabaseSpec which is the connection path to the database
+    /// </summary>
     [Required]
     public string? Host { get; set; }
+    /// <summary>
+    /// Dialect is an element of DatabaseSpec which is the the dialect of the database
+    /// </summary>
     [Required]
     public string? Dialect { get; set; }
+    /// <summary>
+    /// UsernameSecretName is an element of DatabaseSpec which is the username to connect to the database
+    /// </summary>
     [Required]
     public string? UsernameSecretName { get; set; }
+    /// <summary>
+    /// PasswordSecretName is an element of DatabaseSpec which is the password to connect to the database
+    /// </summary>
     [Required]
     public string? PasswordSecretName { get; set; }
+    /// <summary>
+    /// Models is a list elements of type ModelSpec
+    /// </summary>
     [Required]
     public List<ModelSpec> Models { get; set; }
 }
@@ -43,5 +61,8 @@ public struct DatabaseSpec
     PluralName = "data")]
 public class V1Alpha1DataEntity : CustomKubernetesEntity
 {
+    /// <summary>
+    /// Spec is a list elements of type DatabaseSpec
+    /// </summary>
     public List<DatabaseSpec>? Spec { get; set; }
 }
