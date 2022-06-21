e7c532c36247824a6a5760a916e4252fe33d9486
Baban Faraj <baban.faraj@recro.com>
2022-06-21 13:52:04 -0400

    Add comments to ColumnSpec

--- src/Medulla/Kubernetes/Operator/model/database/ColumnSpec.cs
+++ src/Medulla/Kubernetes/Operator/model/database/ColumnSpec.cs
@@ -9,15 +9,46 @@ namespace Operator.model
     /// </summary>
     public struct ColumnSpec
     {
+        /// <summary>
+        /// ColumnName is an element of ColumnSpec which is the column name
+        /// </summary>
         public string? ColumnName { get; set; }
+        /// <summary>
+        /// Type is an element of ColumnSpec which is the column type
+        /// </summary>
         public string? Type { get; set; }
+        /// <summary>
+        /// AllowNull is an element of ColumnSpec which indicates whether or not null is allowed
+        /// </summary>
         public bool AllowNull { get; set; }
+        /// <summary>
+        /// DefaultValue is an element of ColumnSpec which should be set to what the default value of the column should
+        /// be if it exists
+        /// </summary>
         public string? DefaultValue { get; set; }
+        /// <summary>
+        /// AutoIncrement is an element of ColumnSpec which indicates whether auto increment is allowed or not
+        /// </summary>
         public bool AutoIncrement { get; set; }
+        /// <summary>
+        /// PrimaryKey is an element of ColumnSpec which indicates whether this column is the primary key
+        /// </summary>
         public bool PrimaryKey { get; set; }
+        /// <summary>
+        /// Field is an element of ColumnSpec which is the field name
+        /// </summary>
         public string? Field { get; set; }
+        /// <summary>
+        /// Unique is an element of ColumnSpec which is indicates whether or not this column is unique
+        /// </summary>
         public string? Unique { get; set; }
+        /// <summary>
+        /// Comment is an element of ColumnSpec which is a comment of this column
+        /// </summary>
         public string? Comment { get; set; }
+        /// <summary>
+        /// ValidateSpec? is an element of ColumnSpec which is a type ValidateSpec which validates fields
+        /// </summary>
         public ValidateSpec? Validate { get; set; }
 
     }
