5ea066936e18f8548ed8623e5fe6bcbe776a083a
Baban Faraj <baban.faraj@recro.com>
2022-06-21 11:05:42 -0400

    Fix comment for applciation entity

--- src/Medulla/Kubernetes/Operator/Entities/V1Alpha1ApplicationEntity.cs
+++ src/Medulla/Kubernetes/Operator/Entities/V1Alpha1ApplicationEntity.cs
@@ -20,7 +20,7 @@ public struct ApplicationSpec
     [Required]
     public string? Name { get; set; }
     /// <summary>
-    /// Description is is an element of the ApplicationSpec which is the name of the Application created by Medulla
+    /// Description is is an element of the ApplicationSpec which is the description of the Application created by Medulla
     /// </summary>
     [Required]
     public string? Description { get; set; }
@@ -37,6 +37,9 @@ public struct ApplicationSpec
     PluralName = "application")]
 public class V1Alpha1ApplicationEntity : CustomKubernetesEntity
 {
+    /// <summary>
+    /// Spec is is an element of the V1Alpha1ApplicationEntity  which is a list of type ApplicationSpec
+    /// </summary>
     public List<ApplicationSpec>? Spec { get; set; }
 }
 
