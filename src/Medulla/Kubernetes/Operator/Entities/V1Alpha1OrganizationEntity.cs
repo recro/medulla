abb079d2a0a96bf19f79683844039baf01d77726
Baban Faraj <baban.faraj@recro.com>
2022-06-23 11:21:35 -0400

    Add organization

---
+++ src/Medulla/Kubernetes/Operator/Entities/V1Alpha1OrganizationEntity.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+using k8s.Models;
+using KubeOps.Operator.Entities;
+using KubeOps.Operator.Entities.Annotations;
+using KubeOps.Operator.Entities.Extensions;
+using Medulla.Kubernetes.Operator.Model.Organization;
+
+namespace Medulla.Kubernetes.Operator.Entities;
+
+
+
+/// <summary>
+/// V1Alpha1DataEntity is CRD for organization
+/// </summary>
+[Description("A CustomResourceDefinition which allows building with organization in Medulla.")]
+[KubernetesEntity(
+    ApiVersion = "v1alpha1",
+    Kind = "Organization",
+    Group = "medulla.io",
+    PluralName = "organization")]
+public class V1Alpha1OrganizationEntity : CustomKubernetesEntity
+{
+    /// <summary>
+    /// Spec is a list elements of type Organization
+    /// </summary>
+    public List<Organization> Spec { get; set; } = new List<Organization>();
+}
---
+++ src/Medulla/Kubernetes/Operator/Model/Organization/Organization.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+using KubeOps.Operator.Entities.Annotations;
+
+namespace Medulla.Kubernetes.Operator.Model.Organization;
+
+/// <summary>
+/// Spec of CRD for Organization
+/// </summary>
+public struct Organization
+{
+    /// <summary>
+    /// ModelSpec constructor
+    /// </summary>
+    public Organization(){}
+    /// <summary>
+    /// Name is an element of ModelSpec which is the name of the model/table
+    /// </summary>
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+}
