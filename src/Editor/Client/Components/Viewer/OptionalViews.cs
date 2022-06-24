c31f87219542c8df66d130f96d5ec3516849701a
Keith Becker <keith.becker@recro.com>
2022-06-23 18:51:52 -0600

    WorkflowDesigner

---
+++ src/Editor/Client/Components/Viewer/OptionalViews.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+namespace Medulla.Editor.Client.Components.Viewer;
+
+/// <summary>
+/// Enum for which view is active
+/// </summary>
+public enum WhichOption
+{
+
+    /// <summary>
+    /// Editor view
+    /// </summary>
+    Editor,
+    /// <summary>
+    /// Workflow view
+    /// </summary>
+    WorkflowDesigner
+}
+
+/// <summary>
+/// Controls which view is active
+/// </summary>
+public class OptionalViews
+{
+    /// <summary>
+    /// Which enum is active
+    /// </summary>
+    public WhichOption WhichOption { get; set; } = WhichOption.Editor;
+}
