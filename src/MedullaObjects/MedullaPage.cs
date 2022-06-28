abf9c2516c821c3322a483efb1993577a9a22c0a
Keith Becker <keith.becker@recro.com>
2022-06-28 16:24:08 -0600

    Saving to Kubernetes

---
+++ src/MedullaObjects/MedullaPage.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+namespace Operator;
+
+public class MedullaPage : IKubernetesSaveable
+{
+
+    public string PageTitle = "";
+
+    public void SaveToKubernetes()
+    {
+        // load kube config file
+        // KubernetesApi.loadFromLocal()
+
+        // build your crd object
+        // var object = (your object)
+
+        //KubernetesApi.SaveCustomResourceOBject(object)
+
+    }
+}
