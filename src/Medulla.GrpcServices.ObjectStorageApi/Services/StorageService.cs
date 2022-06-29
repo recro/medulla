88a733b6b475974731a62598b09958db8010ddc9
Keith Becker <keith.becker@recro.com>
2022-06-29 12:46:05 -0600

    Working App

--- src/Medulla.GrpcServices.ObjectStorageApi/Services/StorageService.cs
+++ src/Medulla.GrpcServices.ObjectStorageApi/Services/StorageService.cs
@@ -2,9 +2,21 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
+using Grpc.Core;
+
 namespace Medulla.GrpcServices.ObjectStorageApi.Services;
 
-public class StorageService : Medulla.GrpcServices.ObjectStorageApi.
-{
+using ObjectStorageApi;
 
+public class StorageService : Storage.StorageBase
+{
+    public override Task<SaveResponse> SaveObject(SaveRequest request, ServerCallContext context)
+    {
+        return base.SaveObject(request, context);
+    }
+
+    public override Task<GetObjectResponse> GetObject(GetObjectRequest request, ServerCallContext context)
+    {
+        return base.GetObject(request, context);
+    }
 }
