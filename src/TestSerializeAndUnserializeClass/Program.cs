eccc8e172562946cef4cc022313e4b979a4178f3
Keith Becker <keith.becker@recro.com>
2022-06-30 10:44:02 -0600

    Test App

--- src/TestSerializeAndUnserializeClass/Program.cs
+++ src/TestSerializeAndUnserializeClass/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+[Serializable]
+class Test
+{
+    public string TestField = "Test";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
