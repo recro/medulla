1998c01a01f17a5c07479cc7901ebdaf5cb15a1c
Keith Becker <keith.becker@recro.com>
2022-06-30 10:50:58 -0600

    Test App

--- src/TestSerializeAndUnserializeClass/Program.cs
+++ src/TestSerializeAndUnserializeClass/Program.cs
@@ -1,10 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.Serialization.Formatters.Binary;
+
+var test = new Test();
+
+using (var stream = new MemoryStream())
+{
+    var formatter = new BinaryFormatter();
+    formatter.Ser
+}
+
+
+
+
+
 [Serializable]
 class Test
 {
     public string TestField = "Test";
 }
+
+
+
 
 
 
