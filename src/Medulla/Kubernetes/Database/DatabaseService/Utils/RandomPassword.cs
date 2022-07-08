e979769c6045c6488da6580ac36acad27497ae84
Keith Becker <keith.becker@recro.com>
2022-07-08 09:21:35 -0600

    Working App

--- src/Medulla/Kubernetes/Database/DatabaseService/Utils/RandomPassword.cs
+++ src/Medulla/Kubernetes/Database/DatabaseService/Utils/RandomPassword.cs
@@ -1,7 +1,7 @@
 namespace DatabaseService.Utils;
 public class RandomPassword
 {
-    
+
     public static string RandomString(int length)
     {
         var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -15,7 +15,10 @@ public class RandomPassword
 
         var randomString = stringChars.ToString();
 
+        if (randomString == null)
+            throw new Exception("random string is null");
+
         return randomString;
     }
-    
+
 }
