f692987247b1d56bdf000b600a705fd27104884a
Keith Becker <keith.becker@recro.com>
2022-07-14 17:24:04 -0600

    Working App

--- src/Editor/Client/Components/Properties/Types/DatabaseTable.cs
+++ src/Editor/Client/Components/Properties/Types/DatabaseTable.cs
@@ -10,11 +10,50 @@ public class Column
     public string Name { get; set; } = "";
     public string Type { get; set; } = "";
     public bool IsUnique { get; set; } = false;
+
+    public void Overwrite(Column column)
+    {
+        Id = column.Id;
+        Name = column.Name;
+        Type = column.Type;
+        IsUnique = column.IsUnique;
+    }
 }
 
 public class DatabaseTable
 {
-    public string Name { get; set; } = "";
+    public string Id { get; set; }
+    public string Name { get; set; }
     public List<Column> Columns { get; set; } = new();
+
+    public DatabaseTable()
+    {
+        Id = Guid.NewGuid().ToString();
+        Name = "New Table";
+    }
+
+    public void Overwrite(DatabaseTable table)
+    {
+        Id = table.Id;
+        Name = table.Name;
+        foreach (var tableColumn in table.Columns)
+        {
+            if (Columns.Exists(t => t.Id == tableColumn.Id))
+            {
+                foreach (var column in Columns)
+                {
+                    if (column.Id == tableColumn.Id)
+                    {
+                        column.Overwrite(tableColumn);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Columns.Add(tableColumn);
+            }
+        }
+    }
 }
 
---
+++ src/Editor/Client/Library/Database/Database.cs
@@ -0,0 +1,67 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+using Medulla.Editor.Client.Components.Properties.Types;
+
+namespace Medulla.WorkflowDesigner.Client.Library;
+
+/*public class Column
+{
+    public string? Id { get; set; }
+    public string Field { get; set; } = "";
+    public string Type { get; set; } = "string";
+    public bool Unique { get; set; } = false;
+    public bool Required { get; set; } = false;
+}*/
+
+/*public class DatabaseTable
+{
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public List<Column> Columns { get; set; } = new();
+
+}*/
+
+public class Database
+{
+    private static Database? _database;
+
+    private Database()
+    {
+    }
+
+    public static Database GetDatabase()
+    {
+        if (_database == null)
+        {
+            _database = new Database();
+        }
+        return _database;
+    }
+
+
+    public string? Id { get; set; }
+    private DatabaseTable? ActiveEditingTable { get; set; }
+    private List<DatabaseTable> Tables { get; set; } = new();
+
+    public void SaveTable(DatabaseTable table)
+    {
+        var t = Tables.Find(t => t.Id == table.Id);
+        if (t != null)
+        {
+            t.Overwrite(table);
+        }
+        else
+        {
+            Tables.Add(table);
+        }
+    }
+
+    public void SyncTablesWithBackend()
+    {
+
+    }
+
+
+}
