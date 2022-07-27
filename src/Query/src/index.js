d9c8089a9776f86afd46a99cdb5251fe95276968
Keith Becker <keith.becker@recro.com>
2022-07-27 08:51:26 -0600

    Working App

--- src/Query/src/index.js
+++ src/Query/src/index.js
@@ -1,4 +1,4 @@
-const PROTO_PATH = __dirname + '/../../../protos/route_guide.proto';
+const PROTO_PATH = __dirname + '/../query.proto';
 let grpc = require('@grpc/grpc-js');
 let protoLoader = require('@grpc/proto-loader');
 
@@ -17,6 +17,50 @@ var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 var routeguide = protoDescriptor.routeguide;
 
 
+const Get = ({ request }, call) => {
+    console.log("Get");
+    call("test")
+}
+
+const List = () => {
+    console.log("List");
+    call("test")
+}
+
+const Create = () => {
+    console.log("Create");
+    call("test")
+}
+
+const Update = () => {
+    console.log("Update");
+    call("test")
+}
+
+const Delete = () => {
+    console.log("Delete");
+    call("test")
+}
+
+
+
+function getServer() {
+    var server = new grpc.Server();
+    server.addService(routeguide.RouteGuide.service, {
+        Get,
+        List,
+        Create,
+        Update,
+        Delete
+    });
+    return server;
+}
+
+let routeServer = getServer();
+routeServer.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    console.log("Starting server");
+    routeServer.start();
+});
 
 
 
