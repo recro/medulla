15ba3ffd5d227974d518241f9ddd6f58abdde46d
keithbecker <keithloganbecker94@gmail.com>
2022-08-22 07:39:02 -0600

    working app

--- src/Shell/Client/Program.cs
+++ src/Shell/Client/Program.cs
@@ -2,18 +2,18 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
 using Medulla.Shell.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Grpc.Net.Client;
-using Grpc.Net.Client.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
-var channel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient });
+var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient });
 var client = new GrpcDatabaseService.DatabaseSvc.DatabaseSvcClient(channel);
 
 builder.Services.AddSingleton(client);
