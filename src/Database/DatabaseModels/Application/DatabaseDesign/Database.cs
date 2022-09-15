9f2cf39dbe9c0e8170cb48911b94f56f382359dd
keithbecker <keithloganbecker94@gmail.com>
2022-09-15 16:49:21 -0600

    feat: Adding EfCore Classes to Represent a Database structure.

---
+++ src/Database/DatabaseModels/Application/DatabaseDesign/Database.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the Medulla Contributors under one or more agreements.
+// The Medulla Contributors licenses this file to you under the Apache 2.0 license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseModels.Application.DatabaseDesign;
+
+/// <summary>
+/// Represents a database in medulla.
+/// </summary>
+public class Database : DbContext
+{
+
+}
