719d63750db81ff9059de393bb1b899e5fd59558
keithbecker <keithloganbecker94@gmail.com>
2022-09-15 18:10:44 -0600

    feat: Changed Filename to current dir for sqlite

--- src/Database/DatabaseModels/medullaContext.cs
+++ src/Database/DatabaseModels/medullaContext.cs
@@ -21,7 +21,7 @@ namespace Packt.Shared
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlite("Filename=../medulla.db");
+                optionsBuilder.UseSqlite("Filename=./medulla.db");
             }
         }
 
