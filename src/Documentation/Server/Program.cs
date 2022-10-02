979b8b738ebb68dd021a1f16a9b4df5549ade011
Keith Becker <keith.becker@recro.com>
2022-10-02 13:40:06 -0600

    docker

--- src/Documentation/Server/Program.cs
+++ src/Documentation/Server/Program.cs
@@ -3,40 +3,27 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
+var builder = WebApplication.CreateBuilder(args);
 
-var host = new WebHostBuilder()
-            .ConfigureLogging(options => options.AddConsole())
-            .ConfigureLogging(options => options.AddDebug())
-            // .UseDefaultHostingConfiguration(args)
-            // .UseIISPlatformHandlerUrl()
-            // .UseServer("Microsoft.AspNetCore.Server.Kestrel")
-            // .UseStartup<Startup>()
-            .Build();
+var app = builder.Build();
 
-host.Run();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseHsts();
+}
 
+app.UseBlazorFrameworkFiles(new PathString("/docs"));
+app.UseStaticFiles(new StaticFileOptions()
+{
+    ServeUnknownFileTypes = true,
+});
+app.UseStaticFiles(new PathString("/docs"));
 
-// var builder = WebApplication.CreateBuilder(args);
+app.MapFallbackToFile("/docs/{*path:nonfile}", "docs/index.html");
 
-// var app = builder.Build();
-
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseDeveloperExceptionPage();
-//     app.UseWebAssemblyDebugging();
-// }
-// else
-// {
-//     app.UseHsts();
-// }
-
-// app.UseBlazorFrameworkFiles(new PathString("/docs"));
-// app.UseStaticFiles(new StaticFileOptions()
-// {
-//     ServeUnknownFileTypes = true,
-// });
-// app.UseStaticFiles(new PathString("/docs"));
-
-// app.MapFallbackToFile("/docs/{*path:nonfile}", "docs/index.html");
-
-// app.Run();
+app.Run();
