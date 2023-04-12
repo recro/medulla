cf1447f1584626e2081ff6f656647b6b991a8b36
Andrew Babbitt <andrew.babbitt@recro.com>
2023-04-12 09:49:55 -0600

    chore: rename client namespaces

--- src/Client/Shell/Program.cs
+++ src/Client/Shell/Program.cs
@@ -2,11 +2,12 @@
 // The Medulla Contributors licenses this file to you under the Apache 2.0 license.
 // See the LICENSE file in the project root for more information.
 
-using Medulla.Shell.Client;
+using Medulla.Client.Shell;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
